b7b8214edc266c3625479c2b0034af71daab1234
Bob <tekaddict6@gmail.com>
2024-09-17 20:29:51 -0500

    Inital laptop config commit

---
+++ configuration.nix
@@ -0,0 +1,283 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the config from hardware prior
+      <hardware-g713pi/asus/rog-strix/g713pi>
+      # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.initrd.luks.devices."luks-5bdceca8-e2db-4e79-9224-d7d71bba9966".device = "/dev/disk/by-uuid/5bdceca8-e2db-4e79-9224-d7d71bba9966";
+  networking.hostName = "deckard"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Chicago";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = ""; # Colemak varrient ad somehow
+    # options = "grp:alt_shift_toggle";
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.bobw = {
+    isNormalUser = true;
+    description = "BobW";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+
+  # Asus Required
+  services.supergfxd.enable = true;
+  services = {
+    asusd = {
+      enable = true;
+      enableUserService = true;
+    };
+  };
+
+  # Enable Nvidia GPU
+  
+    # Enable OpenGL
+    hardware.opengl = {
+      enable = true;
+    };
+
+    # Load nvidia driver for Xorg and Wayland
+    services.xserver.videoDrivers = ["nvidia"];
+
+    hardware.nvidia = {
+
+      # Modesetting is required.
+      modesetting.enable = true;
+
+      # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+      # Enable this if you have graphical corruption issues or application crashes after waking
+      # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+      # of just the bare essentials.
+      powerManagement.enable = false;
+
+      # Fine-grained power management. Turns off GPU when not in use.
+      # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+      powerManagement.finegrained = false;
+
+      # Use the NVidia open source kernel module (not to be confused with the
+      # independent third-party "nouveau" open source driver).
+      # Support is limited to the Turing and later architectures. Full list of 
+      # supported GPUs is at: 
+      # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+      # Only available from driver 515.43.04+
+      # Currently alpha-quality/buggy, so false is currently the recommended setting.
+      open = false;
+
+      # Enable the Nvidia settings menu,
+	# accessible via `nvidia-settings`.
+      nvidiaSettings = true;
+
+      # Optionally, you may need to select the appropriate driver version for your specific GPU.
+      package = config.boot.kernelPackages.nvidiaPackages.production;
+    };
+  
+
+  # Hyprland config
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+
+  # Hint Electron apps to use wayland
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+  };
+
+  services.dbus.enable = true;
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gtk
+    ];
+  };
+
+  # Sound
+  sound.enable = true;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+#    alsa.support32bit = true;  # Listed as does not exist
+    pulse.enable = true;
+  };
+
+  # Steam
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = false;
+    localNetworkGameTransfers.openFirewall = true;
+  };
+
+  # VMWare
+  virtualisation.vmware.host.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    
+  # Asus
+    supergfxctl
+    asusctl
+
+  # Hyprland
+    hyprland
+    mpvpaper # For wallpapers
+    xdg-desktop-portal-gtk
+    xdg-desktop-portal-hyprland
+    xwayland
+    wayland-protocols
+    wayland-utils
+    wl-clipboard
+    wlroots
+    wofi # App Launcher
+    hyprlock
+    kdePackages.dolphin
+
+  # Sound
+    pavucontrol
+    pipewire
+
+  # Browsers
+    firefox
+    # tor  # Didnt install
+    # ungoogled-chromium # Keeps crashing
+
+  # Office Suite
+    # libreoffice-still
+    libreoffice-qt-fresh
+
+  # Utilities
+    git
+    kitty
+    networkmanagerapplet
+    wget
+    eza
+    btop
+    gh
+    ani-cli
+    ytmdl
+    yewtube
+    drive
+    sl
+    rink
+    nvtopPackages.nvidia
+
+  # Games
+    steam
+    modrinth-app
+    bastet
+    ninvaders
+    dwarf-fortress-packages.dwarf-fortress-full
+
+  # Apps
+    vscode
+    kuro # Issue with electron version
+    ferdium # Messaging app
+    kicad
+    # github-desktop
+    pwsafe
+    logseq # Uses Insecure Electron version and breaks often
+    evolution
+    vmware-workstation
+    vmware-horizon-client
+    ollama
+    blender
+    freecad
+    openscad
+  ];
+
+  nixpkgs.config.permittedInsecurePackages = [
+    "electron-27.3.11"
+    "electron-29.4.6"
+  ];
+
+  nixpkgs.overlays = [
+    (
+      final: prev: {
+        logseq = prev.logseq.overrideAttrs (oldAttrs: {
+          postFixup = ''
+            makeWrapper ${prev.electron_27}/bin/electron $out/bin/${oldAttrs.pname} \
+              --set "LOCAL_GIT_DIRECTORY" ${prev.git} \
+              --add-flags $out/share/${oldAttrs.pname}/resources/app \
+              --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}" \
+              --prefix LD_LIBRARY_PATH : "${prev.lib.makeLibraryPath [ prev.stdenv.cc.cc.lib ]}"
+          '';
+        });
+      }
+    )
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+}
