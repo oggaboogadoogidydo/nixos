a30606b8fe2269cd4b721cb80d0856a8bd20b4f9
System administrator <tekaddict6@gmail.com>
2025-11-02 00:15:01 -0500

    installed Yazi as new file browser. Now also changed timeout alert to 60 sec

--- configuration.nix
+++ configuration.nix
@@ -300,7 +300,54 @@
     bash = {
       enableLsColors = true;  # Enable colors for ls
       undistractMe.enable = true;
+      undistractMe.timeout = 60;
       undistractMe.playSound = true;
+    };
+
+    # Yazi Terminal File Manager
+    yazi = {
+      enable = true;
+      settings = {
+        yazi = {
+          ratio = [
+            1
+            4
+            3
+          ];
+          sort_by = "natural";
+          sort_sensitive = true;
+          sort_reverse = false;
+          sort_dir_first = true;
+          linemode = "size";
+          show_hidden = true;
+          show_symlink = true;
+
+        preview = {
+          wrap = "yes";
+          image_filter = "lanczos3";
+          image_quality = 90;
+          tab_size = 1;
+          max_width = 600;
+          max_height = 900;
+          cache_dir = "";
+          ueberzug_scale = 1;
+          ueberzug_offset = [
+            0
+            0
+            0
+            0
+          ];
+        };
+
+        tasks = {
+          micro_workers = 5;
+          macro_workers = 10;
+          bizarre_retry = 5;
+        };
+      };
+      theme = {};
+      keymap = {};
+      };
     };
   };
 
